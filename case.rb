2fb9f1f7b5d79289670d0ce09e26adabbb930a7b
skeskali <cecily.walker@gmail.com>
2014-06-06 21:21:24 -0700

    Added two new ruby scripts.

---
+++ case.rb
@@ -0,0 +1,11 @@
+weather = 'sunny'
+case weather
+  when 'sunny'
+    puts "Yay, it's Sunny! It's a perfect day for a bike ride."
+  when 'raining'
+    puts "Oh well. Good thing you have a rain cape."
+  when 'snowing'
+    puts "It's OK to leave the bike at home once in awhile."
+  when 'foggy'
+    puts "Better make sure your lights are fully charged."
+end
