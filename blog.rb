d93e3deb94d233d23750e52c7204e98891052e03
skeskali <cecily.walker@gmail.com>
2014-08-03 21:22:27 -0700

    Deleted redundant files

--- blog.rb
+++ blog.rb
@@ -1,26 +1,27 @@
 class Blog
-    
+    attr_accessor :title, :all_posts, :total_posts
+
     def initialize
         @created_at = Time.now
-        puts "Give your blog a name:"
+        puts "Name your blog:"
         @title = gets.chomp
-        @all_blog_posts = []
-        @total_blog_posts = 0
+        @all_posts = []
+        @total_posts = 0
     end
 
     def create_blogpost
-        new_blog_post = Blog_Post.new
-        puts "here is my new blog post"
-        @all_blog_posts << new_blog_post
-        @total_blog_posts +=1
-    end 
-
-    def collect_blog_posts
-        return @all_blog_posts
+        new_blog_post = Blog_Post.new
+        puts "Here is my new blog post"
+        @all_posts << new_blog_post
+        @total_posts += 1
     end
 
-    def publish(all_blog_posts)
-        all_blog_posts.each do |blog_post|
+    def list_posts
+        return @all_posts
+    end
+
+    def publish(all_posts)
+        all_posts.each do |blog_post|
             puts blog_post.title
             puts blog_post.created_at
             puts blog_post.content
@@ -29,20 +30,21 @@ class Blog
 end
 
 class Blog_Post
+    attr_accessor :title, :created_at, :content
 
-    def initialize 
-        @created_at = Time.now
-        puts "Give your blog post a name:"
+    def initialize
+        @created_at = Time.new
+        puts "Name your blog post:"
         @title = gets.chomp
 
-        puts "Enter your blog post content:"
+        puts "Your blog post content:"
         @content = gets.chomp
     end
 
-    def edit_blog_content
+    def edit_content
         puts "New blog title:"
         @title = gets.chomp
-    
+
         puts "New blog text:"
         @content = gets.chomp
     end
@@ -50,6 +52,6 @@ end
 
 my_blog = Blog.new
 first_blog_post = my_blog.create_blogpost
-all_blog_posts = my_blog.collect_blog_posts
+all_posts = my_blog.list_posts
 puts my_blog.inspect
-my_blog.publish(all_blog_posts)
+my_blog.publish(all_posts)
